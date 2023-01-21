952bdce7642ba9af0f093c9212b9acb660b85ae8
Alexander Mityukov <a.m.mityukov@gmail.com>
2023-01-22 00:05:25 +0100

    Rewrite program without classes

--- developers_salaries/developers_salaries.py
+++ developers_salaries/developers_salaries.py
@@ -2,195 +2,129 @@ from environs import Env
 import requests
 from urllib import parse
 from itertools import count
+from collections.abc import Generator, Callable
 from terminaltables import AsciiTable
-from collections.abc import Iterator, Iterable
-from abc import ABC, abstractmethod
 
 
-class Vacancy():
-    def __init__(self,
-                 language: str,
-                 currency: str,
-                 salary_from: int | None,
-                 salary_to: int | None
-                 ) -> None:
-        self.language: str = language
-        self.currency: str = currency
-        self.salary_from: int | None = salary_from
-        self.salary_to: int | None = salary_to
-        self.average_salary: int | None = self._count_average_salary()
-
-    def __str__(self) -> str:
-        return (f'{self.language} - '
-                f'{self.currency} - '
-                f'{self.salary_from} - '
-                f'{self.salary_to} - '
-                f'{self.average_salary}')
-
-    def __repr__(self) -> str:
-        return self.__str__()
-
-    def _count_average_salary(self) -> int | None:
-        match self.currency, self.salary_from, self.salary_to:
-            case 'RUR' | 'rub', int() as salary_from, int() as salary_to:
-                return int((salary_from + salary_to)/2)
-            case 'RUR' | 'rub', None, int() as salary_to:
-                return int(salary_to * 0.8)
-            case 'RUR' | 'rub', int() as salary_from, None:
-                return int(salary_from * 1.2)
-            case _:
-                return None
-
-
-class VacanciesPortal(ABC):
-    def __init__(self, portal_name: str) -> None:
-        self.portal_name: str = portal_name
-        self.vacancies: list = []
-        self.LANGUAGES: Iterable = ()
-
-    def languages_stats_table(self) -> AsciiTable.table:
-        table_rows: list = [('Язык программирования',
-                             'Вакансий найдено',
-                             'Вакансий обработано',
-                             'Средняя зарплата')]
-        for language in self.LANGUAGES:
-            language_stats: tuple | None = self._get_language_stats(language)
-            if language_stats:
-                table_rows.append(language_stats)
-        table_instance: AsciiTable = AsciiTable(table_rows, self.portal_name)
-        return table_instance.table
-
-    def _get_language_stats(self, language: str) -> tuple[str, int, int, int]:
+def get_statistics_table(portal_name: str,
+                         vacancies_salaries_generator: Callable
+                         ) -> AsciiTable.table:
+    table_rows: list = [('Язык программирования',
+                         'Вакансий найдено',
+                         'Вакансий обработано',
+                         'Средняя зарплата')]
+    for language in POPULAR_LANGUAGES:
         found: int = 0
         processed: int = 0
         salaries_sum: int = 0
-        for vacancy in self.vacancies:
-            if vacancy.language == language:
-                found += 1
-                if vacancy.average_salary:
-                    salaries_sum += vacancy.average_salary
-                    processed += 1
+        for salary in vacancies_salaries_generator(language):
+            found += 1
+            if salary:
+                salaries_sum += salary
+                processed += 1
         if found and processed:
-            return language, found, processed, int((salaries_sum/processed))
+            table_rows.append((language,
+                               found,
+                               processed,
+                               int((salaries_sum/processed))))
         elif found:
-            return language, found, 0, 0
+            table_rows.append((language, found, 0, 0))
         else:
-            return language, 0, 0, 0
-
-    @staticmethod
-    @abstractmethod
-    def _fetch_records(url: str,
-                       params: dict,
-                       headers: dict | None) -> Iterator:
-        pass
-
-    @abstractmethod
-    def _get_vacancies_by_language(self, language: str) -> None:
-        pass
+            table_rows.append((language, 0, 0, 0))
+    return AsciiTable(table_rows, portal_name).table
 
 
-class HeadHunter(VacanciesPortal):
-    def __init__(self, languages: Iterable) -> None:
-        super().__init__('HeadHunter')
-        self.API_URL: str = 'https://api.hh.ru/'
-        self.LANGUAGES: Iterable[str] = languages
-        for language in self.LANGUAGES:
-            self._get_vacancies_by_language(language)
+def count_average_rur_salary(currency: str,
+                             salary_from: str,
+                             salary_to: str) -> int | None:
+    match currency, salary_from, salary_to:
+        case 'RUR' | 'rub', int() as salary_from, int() as salary_to:
+            return int((salary_from + salary_to)/2)
+        case 'RUR' | 'rub', None, int() as salary_to:
+            return int(salary_to * 0.8)
+        case 'RUR' | 'rub', int() as salary_from, None:
+            return int(salary_from * 1.2)
+        case _:
+            return None
 
-    @staticmethod
-    def _fetch_records(url: str,
-                       params: dict, headers: dict | None = None) -> Iterator:
-        for page in count():
-            params['page'] = page
-            page_response: requests.Response = requests.get(url=url,
-                                                            params=params,
-                                                            headers=headers)
-            page_response.raise_for_status()
-            page_payload: dict = page_response.json()
-            yield from page_payload['items']
-            if page >= page_payload['pages'] - 1:
-                break
 
-    def _get_vacancies_by_language(self, language: str) -> None:
-        vacancies_url: str = parse.urljoin(self.API_URL, 'vacancies')
-        DEVELOPER_PROFESSION_ROLE_ID: str = '96'
-        MOSCOW_TOWN_ID: str = '1'
-        params: dict[str, str] = {
+def get_hh_vacancies_avg_salary(language: str) -> Generator[int | None,
+                                                            None,
+                                                            None]:
+    API_URL: str = 'https://api.hh.ru/'
+    VACANCIES_URL: str = parse.urljoin(API_URL, 'vacancies')
+    DEVELOPER_PROFESSION_ROLE_ID: str = '96'
+    MOSCOW_TOWN_ID: str = '1'
+    params: dict[str, str | int] = {
             'text': f'Программист {language}',
             'area': MOSCOW_TOWN_ID,
             'professional_role': DEVELOPER_PROFESSION_ROLE_ID,
             'only_with_salary': 'true',
         }
-        for vacancy in self._fetch_records(vacancies_url,
-                                           params):
-            self.vacancies.append(
-                Vacancy(language=language,
-                        currency=vacancy['salary']['currency'],
-                        salary_from=vacancy['salary']['from'],
-                        salary_to=vacancy['salary']['to'])
-                )
+    for page in count():
+        params['page'] = page
+        page_response: requests.Response = requests.get(url=VACANCIES_URL,
+                                                        params=params)
+        page_response.raise_for_status()
+        page_payload: dict = page_response.json()
+        for vacancy in page_payload['items']:
+            vacancy_average_salary: int | None = count_average_rur_salary(
+                                                 vacancy['salary']['currency'],
+                                                 vacancy['salary']['from'],
+                                                 vacancy['salary']['to'])
+            yield vacancy_average_salary
+        if page >= page_payload['pages'] - 1:
+            break
 
 
-class SuperJob(VacanciesPortal):
-    def __init__(self, languages: Iterable, client_secret: str) -> None:
-        super().__init__('SuperJob')
-        self.API_URL: str = 'https://api.superjob.ru'
-        self.LANGUAGES: Iterable[str] = languages
-        self.CLIENT_SECRET: str = client_secret
-        for language in self.LANGUAGES:
-            self._get_vacancies_by_language(language)
-
-    @staticmethod
-    def _fetch_records(url: str,
-                       params: dict,
-                       headers: dict | None) -> Iterator:
-        for page in count():
-            params['page'] = page
-            page_response: requests.Response = requests.get(url=url,
-                                                            params=params,
-                                                            headers=headers)
-            page_response.raise_for_status()
-            page_payload: dict = page_response.json()
-            yield from page_payload['objects']
-            if not page_payload['more']:
-                break
-
-    def _get_vacancies_by_language(self, language: str) -> None:
-        vacancies_url: str = parse.urljoin(self.API_URL, '2.0/vacancies')
-        DEVELOPMENT_CATALOGUE_ID: str = '48'
-        headers: dict[str, str] = {
-            'X-Api-App-Id': self.CLIENT_SECRET,
-        }
-        params: dict[str, str] = {
-            'keyword': f'Программист {language}',
-            'town': 'Москва',
-            'catalogues': DEVELOPMENT_CATALOGUE_ID,
-            'no_agreement': '1',
-            'count': '100',
-        }
-        for vacancy in self._fetch_records(vacancies_url,
-                                           params,
-                                           headers):
-            self.vacancies.append(
-                Vacancy(language=language,
-                        currency=vacancy['currency'],
-                        salary_from=vacancy['payment_from'],
-                        salary_to=vacancy['payment_to'])
-                )
+def get_superjob_vacancies_avg_salary(language: str) -> Generator[int | None,
+                                                                  None,
+                                                                  None]:
+    API_URL: str = 'https://api.superjob.ru'
+    VACANCIES_URL: str = parse.urljoin(API_URL, '2.0/vacancies')
+    DEVELOPMENT_CATALOGUE_ID: str = '48'
+    headers: dict[str, str] = {
+        'X-Api-App-Id': SUPERJOB_CLIENT_SECRET,
+    }
+    params: dict[str, str | int] = {
+        'keyword': f'Программист {language}',
+        'town': 'Москва',
+        'catalogues': DEVELOPMENT_CATALOGUE_ID,
+        'no_agreement': '1',
+        'count': '100',
+    }
+    for page in count():
+        params['page'] = page
+        page_response: requests.Response = requests.get(url=VACANCIES_URL,
+                                                        params=params,
+                                                        headers=headers)
+        page_response.raise_for_status()
+        page_payload: dict = page_response.json()
+        for vacancy in page_payload['objects']:
+            vacancy_average_salary: int | None = count_average_rur_salary(
+                                                 vacancy['currency'],
+                                                 vacancy['payment_from'],
+                                                 vacancy['payment_to'])
+            yield vacancy_average_salary
+        if not page_payload['more']:
+            break
 
 
 if __name__ == '__main__':
     env: Env = Env()
     env.read_env()
+    SUPERJOB_CLIENT_SECRET: str = env('SUPERJOB_CLIENT_SECRET')
     try:
-        with open(env('LANGUAGES_FILE',
-                      default='languages.txt'), 'r') as file:
-            popular_languages: list = [line for line in
-                                       file.read().splitlines()]
+        with open(env('LANGUAGES_FILE', default='languages.txt'), 'r') as file:
+            POPULAR_LANGUAGES: list = [line for line
+                                       in file.read().splitlines()]
     except OSError:
-        print("Can't open file languages file!")
-    hh_portal: HeadHunter = HeadHunter(popular_languages)
-    print(hh_portal.languages_stats_table())
-    superjob: SuperJob = SuperJob(popular_languages,
-                                  env('SUPERJOB_CLIENT_SECRET'))
-    print(superjob.languages_stats_table())
+        print("Can't open languages file!")
+    hh_table: AsciiTable = get_statistics_table(
+                                    'HeadHunter',
+                                    get_hh_vacancies_avg_salary)
+    print(hh_table)
+    superjob_table: AsciiTable = get_statistics_table(
+                                     'SuperJob',
+                                     get_superjob_vacancies_avg_salary)
+    print(superjob_table)
